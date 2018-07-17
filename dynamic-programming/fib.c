bdcab4e74e24b25e28097882eed9071f5fb16b5e
Ramna Sidharta <ramna.thors@gmail.com>
2018-07-17 20:35:48 -0300

    dyn-prog: fibonacci top-bottom and bottom-up approach

--- dynamic-programming/fib.c
+++ dynamic-programming/fib.c
@@ -10,19 +10,25 @@ int fib(int n) {
     return f[n];
 }
 
+int fib2(int n) {
+    for (int i = 2; i <= n; ++i)
+        f[i] = fib2(n-1) + fib2(n-2);
+    return f[n];
+}
+
 void main() {
     int n;
     scanf("%d", &n);
 
     f[0] = 0;
     f[1] = 1;
-    fib(n);
+    fib2(n);
 
     if (n == 0) {
         printf("0\n");
     } else {
         for (int i = 0; i < n-1; ++i)
             printf("%d ", f[i]);
-            printf("%d\n", f[n-1]);
+        printf("%d\n", f[n-1]);
     }
 }
