5824d918abef75757afbebcf7fa0d5258c02da30
Ramna Sidharta <ramna.thors@gmail.com>
2018-07-18 20:21:25 -0300

    melhorando código rod_cutting

--- dynamic-programming/rod_cutting.c
+++ dynamic-programming/rod_cutting.c
@@ -1,39 +1,45 @@
+/*
+ * https://practice.geeksforgeeks.org/problems/rod-cutting/0
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX(a, b) (((a) >= (b)) ? (a) : (b))
+#define LIM 102
 
 void init_mem();
 void resolver_min_moedas();
 
-int t;  // testes
-int n;  // tamanho da tora (array)
-int precos[102];
-int mem[1000];
+int t;            // number of tests
+int len;          // length of the rod
+int precos[LIM];  // prices of the ith rod
+int mem[LIM];     // maximum income of the ith rod
 
-void resolver() {
+void init_mem() {
     mem[0] = 0;
-    for (int i = 1; i <= n; ++i)
+    for (int i = 1; i <= len; ++i)
         mem[i] = precos[i];
+}
 
-    for (int i = 1; i <= n; ++i)
+int max_income() {
+    for (int i = 1; i <= len; ++i)
         for (int k = 0; k <= i; ++k)
             mem[i] = MAX(mem[i-k] + precos[k], mem[i]);
 
-    printf("%d\n", mem[n]);
+    return mem[len];
 }
 
 void main() {
-    int p;
     scanf("%d ", &t);
     for(int i = 0; i < t; ++i) {
-        scanf("%d ", &n);
+        scanf("%d ", &len);
 
-        for (int j = 1; j <= n; ++j) {
-            scanf("%d ", &p);
-            precos[j] = p;
-        }
+        for (int j = 1; j <= len; ++j)
+            scanf("%d ", &precos[j]);
 
-        resolver();
+        init_mem();
+        printf("%d\n", max_income());
     }
 }
